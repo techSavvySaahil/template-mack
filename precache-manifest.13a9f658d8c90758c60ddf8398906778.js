3ee12270b117a2c8889eba037bd98195ce708aec
techSavvySaahil <saahil.madaan@gmail.com>
2020-08-16 13:40:08 +0530

    Updates

---
+++ precache-manifest.13a9f658d8c90758c60ddf8398906778.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3d67b5257467e5b9c242bc5fbd61eb",
+    "url": "/template-mack/index.html"
+  },
+  {
+    "revision": "37578269033654610e0c",
+    "url": "/template-mack/static/css/main.0e04accc.chunk.css"
+  },
+  {
+    "revision": "c53bc2c256978ef182d0",
+    "url": "/template-mack/static/js/2.6f88618d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/template-mack/static/js/2.6f88618d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37578269033654610e0c",
+    "url": "/template-mack/static/js/main.20af6804.chunk.js"
+  },
+  {
+    "revision": "381c3e475ef1dbae54ee",
+    "url": "/template-mack/static/js/runtime-main.5548c94a.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/template-mack/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/template-mack/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "798260c481afa4ae4703f15fcfc06ec7",
+    "url": "/template-mack/static/media/boxer-brief.798260c4.svg"
+  },
+  {
+    "revision": "b0b52735b0ab821620b6e0a66aa7370e",
+    "url": "/template-mack/static/media/boxer-brief.b0b52735.svg"
+  },
+  {
+    "revision": "64795d8c730feedcc6100375747058de",
+    "url": "/template-mack/static/media/breathable.64795d8c.svg"
+  },
+  {
+    "revision": "8c833a85fd783136c17a75bb71a335c7",
+    "url": "/template-mack/static/media/breathable.8c833a85.svg"
+  },
+  {
+    "revision": "01b508bd91bd91670dd6bfc3dcea4a96",
+    "url": "/template-mack/static/media/brief.01b508bd.svg"
+  },
+  {
+    "revision": "dcfb1254942961e29fd20b0b9b425aff",
+    "url": "/template-mack/static/media/brief.dcfb1254.svg"
+  },
+  {
+    "revision": "2688e08483292d91efa7d8f3b5085d04",
+    "url": "/template-mack/static/media/description.2688e084.svg"
+  },
+  {
+    "revision": "ecb18bee87c98a0d0bf182cd2f304342",
+    "url": "/template-mack/static/media/description.ecb18bee.svg"
+  },
+  {
+    "revision": "629da88493508e63bc0ff5941a89ffc8",
+    "url": "/template-mack/static/media/eight-boxer-brief.629da884.svg"
+  },
+  {
+    "revision": "d69194d34946f2abc4613dbfd5386f6f",
+    "url": "/template-mack/static/media/eight-boxer-brief.d69194d3.svg"
+  },
+  {
+    "revision": "2846fefa70d02107831029e82eba5aad",
+    "url": "/template-mack/static/media/hero.2846fefa.svg"
+  },
+  {
+    "revision": "930a77555e3d00e4479a15df18161258",
+    "url": "/template-mack/static/media/hero.930a7755.svg"
+  },
+  {
+    "revision": "d901122dfee2715f748cd3902ca8a253",
+    "url": "/template-mack/static/media/logo.d901122d.png"
+  },
+  {
+    "revision": "1ea543402a1a8e9e4a074f5a2ac713be",
+    "url": "/template-mack/static/media/trunk.1ea54340.svg"
+  },
+  {
+    "revision": "f03cff49d465a470aeaf15ba10ad0241",
+    "url": "/template-mack/static/media/trunk.f03cff49.svg"
+  }
+]);
